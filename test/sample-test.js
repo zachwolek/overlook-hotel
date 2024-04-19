61e0c9aed7b552b175e1b12d820f646862071f58
Zach Wolek <zach.wolek@gmail.com>
2024-04-19 11:37:53 -0500

    Create the main page

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 const expect = chai.expect;
+import { verifyID } from "/..src/userFunctions"
 
 describe('See if the tests are running', function() {
   it('should return true', function() {
@@ -7,4 +8,21 @@ describe('See if the tests are running', function() {
   });
 });
 
+describe("Verify ID", () => {
+  it.skip("Should allow access to the correct username and password", () => {
 
+     expect(recipe[0]).to.deep.equal(constants.wingSauce);
+   });
+
+   it.skip("Should notify if the password is incorrect", () => {
+
+    expect(recipe).to.deep.equal(constants.saladArray);
+  });
+   
+  it.skip("Should notify if the information is incomplete", () => {
+  
+    expect(recipe).to.deep.equal(constants.saladArray);
+  });
+   
+
+  });
